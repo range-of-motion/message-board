b29f1c557976355d47396a937ebb998f83255b55
Daniël van Drunen <daniel@pixely.me>
2018-01-06 00:48:48 +0100

    Add model and migration for threads

---
+++ database/migrations/2018_01_05_233848_create_threads_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration {
+    public function up() {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('threads');
+    }
+}
