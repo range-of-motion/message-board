0210f3b5635957a092a5086dd93a95b432eb9b92
Daniël van Drunen <daniel@pixely.me>
2018-02-10 01:56:11 +0100

    Whitelist certain fields for Comment and Thread models (to prevent sensitive data from showing up unexpectedly; thanks Jasper)

--- app/Comment.php
+++ app/Comment.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use App\Thread;
 
 class Comment extends Model {
+    protected $visible = ['id', 'created_at', 'thread_id', 'message'];
+
     public function thread() {
         return $this->belongsTo(Thread::class);
     }
--- app/Thread.php
+++ app/Thread.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use App\Comment;
 
 class Thread extends Model {
+    protected $visible = ['id', 'created_at', 'title'];
+
     public function comments() {
         return $this->hasMany(Comment::class);
     }
