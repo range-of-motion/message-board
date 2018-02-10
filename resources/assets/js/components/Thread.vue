b0972eab2a90efce7402f31b84478fd6047a090f
Daniël van Drunen <daniel@pixely.me>
2018-02-10 02:29:16 +0100

    Add collapsable comments to Thread component

--- resources/assets/js/components/Thread.vue
+++ resources/assets/js/components/Thread.vue
@@ -1,8 +1,14 @@
 <template>
     <li>
-        {{ thread.created_at }} &middot; {{ thread.title }}
+        <div>{{ thread.created_at }} &middot; {{ thread.title }}</div>
+        <div class="mt-2" v-if="collapsedComments && thread.comments.length > 3">
+            <button class="wide" @click="toggleCollapse">Show {{ thread.comments.length - 3 }} more</button>
+        </div>
+        <div class="mt-2" v-if="!collapsedComments && thread.comments.length > 3">
+            <button class="wide" @click="toggleCollapse">Hide {{ thread.comments.length - 3 }} again</button>
+        </div>
         <ul class="comments">
-            <li v-for="comment in thread.comments">{{ comment.created_at }} &middot; {{ comment.message }}</li>
+            <li v-for="(comment, index) in thread.comments" v-if="!collapsedComments || index >= thread.comments.length - 3">{{ comment.created_at }} &middot; {{ comment.message }}</li>
         </ul>
         <comment-composer v-bind:thread="thread.id"></comment-composer>
     </li>
@@ -10,6 +16,18 @@
 
 <script>
     export default {
-        props: ['thread']
+        props: ['thread'],
+
+        data() {
+            return {
+                collapsedComments: true
+            }
+        },
+
+        methods: {
+            toggleCollapse: function () {
+                this.collapsedComments = !this.collapsedComments
+            }
+        }
     }
 </script>
