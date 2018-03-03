5a0302747dbe3fd07f49e8f48e3b7598f3dd4f29
Daniël van Drunen <daniel@pixely.me>
2018-03-03 21:58:16 +0100

    Change format for Comment component so that it lines up with Thread's layout

--- resources/assets/js/components/Comment.vue
+++ resources/assets/js/components/Comment.vue
@@ -1,11 +1,18 @@
 <template>
     <li>
-        {{ comment.created_at }} &middot; {{ comment.message }}
+        <div>{{ blyat }} &middot; VOTE_COUNT_PLACEHOLDER</div>
+        <div style="margin-top: 24px;">{{ comment.message }}</div>
     </li>
 </template>
 
 <script>
     export default {
-        props: ['comment']
+        props: ['comment'],
+
+        computed: {
+            blyat: function () {
+                return moment(this.comment.created_at).format('DD-MM HH:mm')
+            }
+        }
     }
 </script>
